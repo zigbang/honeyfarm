30736371bf23613201ce831b5b000c023e76bc47
arthur_zigbang <arthur@zigbang.com>
2020-12-08 14:52:58 +0900

    showInDashboard가 true일 경우 dashboard에서 DASHBOARD 라고 표기

--- packages/master/src/modules/dashboard/controller.ts
+++ packages/master/src/modules/dashboard/controller.ts
@@ -23,11 +23,12 @@ export class DashboardController {
 
 		Object.entries(data).filter(([key, value]) => {
 			const nodeAddress = key.split(":")[0]
+			const status = value.showInDashboard ? "DASHBOARD" : value.status
 			if(value.platform === "android" && nodeAddress) {
 				const view_container_html = `
 				<div class="view_container">
 					<div class="title_view">
-						<text class="title">${value.name} (${value.status})</text>
+						<text class="title">${value.name} (${status})</text>
 					</div>
 					<iframe height="750px" width="500px" src="http://${nodeAddress}:${wsPort}/#!action=stream&udid=${value.udid}&decoder=broadway&ip=${nodeAddress}&port=${wsPort}&query=%3Faction%3Dproxy%26remote%3Dtcp%253A8886%26udid%3D${value.udid}" frameBorder="0"></iframe>
 				</div>
